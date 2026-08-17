0f18d6e49d4d8374ba38b55b6c5adec1c36361d5
Derrik Diener <soltros@proton.me>
2026-08-17 17:46:29 -0400

    Set Pantheon dock favorites via systemd user service

--- flake.nix
+++ flake.nix
@@ -70,26 +70,16 @@
             services.xserver.displayManager.lightdm.enable = true;
             services.desktopManager.pantheon.enable = true;
 
-            # Dock favorites
-            programs.dconf.profiles.user.databases = [
-              {
-                settings."io/elementary/dock".launchers = [
-                  "firefox.desktop"
-                  "thunderbird.desktop"
-                  "discord.desktop"
-                  "org.signal.Signal.desktop"
-                  "im.fluffychat.Fluffychat.desktop"
-                  "io.github.victoralvesf.aonsoku.desktop"
-                  "steam.desktop"
-                  "com.heroicgameslauncher.hgl.desktop"
-                  "geany.desktop"
-                  "io.elementary.code.desktop"
-                  "Zettlr.desktop"
-                  "io.elementary.files.desktop"
-                  "io.elementary.terminal.desktop"
-                ];
-              }
-            ];
+            # Dock favorites via user dconf
+            systemd.user.services.set-dock-favorites = {
+              description = "Set Pantheon dock favorites";
+              wantedBy = [ "graphical-session.target" ];
+              serviceConfig.Type = "oneshot";
+              serviceConfig.RemainAfterExit = true;
+              script = ''
+                dconf write /io/elementary/dock/launchers "['firefox.desktop', 'thunderbird.desktop', 'discord.desktop', 'org.signal.Signal.desktop', 'im.fluffychat.Fluffychat.desktop', 'io.github.victoralvesf.aonsoku.desktop', 'steam.desktop', 'com.heroicgameslauncher.hgl.desktop', 'geany.desktop', 'io.elementary.code.desktop', 'Zettlr.desktop', 'io.elementary.files.desktop', 'io.elementary.terminal.desktop']"
+              '';
+            };
 
             # Enable Flatpak support
             services.flatpak.enable = true;
