af2e76ed8e6f1ebe26f0df09effcb864b8b100aa
Derrik Diener <soltros@proton.me>
2026-09-19 16:50:27 -0400

    feat: enable quick-settings-tray and update soltros-nixpkgs overlay

--- flake.nix
+++ flake.nix
@@ -223,6 +223,7 @@
         ripgrep
         fd
         duf
+	quick-settings-tray
         antigravity-nix.packages.x86_64-linux.default
         antigravity-nix.packages.x86_64-linux.google-antigravity-ide
         antigravity-nix.packages.x86_64-linux.google-antigravity-cli
